9d8724c5d087e5664178eeda34a2596ce5e392a2
DavidRuff06 <90778233+DavidRuff06@users.noreply.github.com>
2022-09-09 12:12:24 -0500

    Made Working Project
    -Use more dimin values

--- app/src/main/java/com/example/themepractice/MainActivity.java
+++ app/src/main/java/com/example/themepractice/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.themepractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void guessing(View v) {
+    EditText viewGuess = findViewById(R.id.userGuess);
+    TextView answerText = findViewById(R.id.answer);
+    String guess = viewGuess.getText().toString();
+
+    if(guess.equalsIgnoreCase(("fries"))){
+        answerText.setText("CORRECT!!");
+        } else {
+            answerText.setText("Try again");
+        }
+    }
+
 }
