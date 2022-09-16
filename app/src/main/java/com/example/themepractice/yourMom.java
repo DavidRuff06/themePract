3a3df9a73fabf6de8184c5ff099c26a9c5ba7401
DavidRuff06 <90778233+DavidRuff06@users.noreply.github.com>
2022-09-16 12:58:24 -0500

    Buttons layout not correct

---
+++ app/src/main/java/com/example/themepractice/yourMom.java
@@ -0,0 +1,14 @@
+package com.example.themepractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class yourMom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_mom);
+    }
+}
